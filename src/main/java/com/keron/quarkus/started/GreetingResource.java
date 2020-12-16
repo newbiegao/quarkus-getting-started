ac55951116c1a0cf65811304e6f07bb9cf6d2881
gaolk <newbiegao@hotmail.com>
2020-12-16 17:39:45 +0800

    init project

---
+++ src/main/java/com/keron/quarkus/started/GreetingResource.java
@@ -0,0 +1,28 @@
+package com.keron.quarkus.started;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    private HelloService helloService ;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/polite/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return helloService.politeHello(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
